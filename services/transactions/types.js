0a78af80215e9cd0e03cccf38d760a20c36d2c4c
trangpham-jh <trang.pham@journeyh.io>
2020-11-05 14:08:39 +0700

    Change logic of custom pricing

--- services/transactions/types.js
+++ services/transactions/types.js
@@ -3,6 +3,8 @@ export const LINE_ITEM_DAY = 'line-item/day';
 export const LINE_ITEM_UNITS = 'line-item/units';
 export const LINE_ITEM_CUSTOMER_COMMISSION = 'line-item/customer-commission';
 export const LINE_ITEM_PROVIDER_COMMISSION = 'line-item/provider-commission';
+export const LINE_ITEM_DELIVERY = 'line-item/delivery';
+
 
 export const LINE_ITEMS = [
   LINE_ITEM_NIGHT,
@@ -10,4 +12,5 @@ export const LINE_ITEMS = [
   LINE_ITEM_UNITS,
   LINE_ITEM_CUSTOMER_COMMISSION,
   LINE_ITEM_PROVIDER_COMMISSION,
+  LINE_ITEM_DELIVERY,
 ];
