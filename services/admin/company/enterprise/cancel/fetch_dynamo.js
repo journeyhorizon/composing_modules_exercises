925e6edfde65e3942b6df78c4ceffa61d510adc7
trangpham-jh <trang.pham@journeyh.io>
2020-12-14 17:44:11 +0700

    Fix bug: handling when scaned data is reach 1MB

--- services/admin/company/enterprise/cancel/fetch_dynamo.js
+++ services/admin/company/enterprise/cancel/fetch_dynamo.js
@@ -1,8 +1,10 @@
 import config from '../../../../config';
 import dynamoDBSdk from '../../../../../services/dynamoDB';
 
-const fetchAllDataInDynamoDB = async () => {
-  return dynamoDBSdk(config.aws.dynamodb.tableName).scan();
+const fetchAllDataInDynamoDB = async (lastKey) => {
+  return dynamoDBSdk(config.aws.dynamodb.tableName).scan({
+    startKey: lastKey
+  });
 }
 
 export default fetchAllDataInDynamoDB;
--- services/admin/company/enterprise/fetch_current_user.js
+++
@@ -1,7 +0,0 @@
-import { getUserData } from "../../../sharetribe_admin";
-
-const fetchUser = async (userId) => {
-  return getUserData({ userId })
-}
-
-export default fetchUser;
