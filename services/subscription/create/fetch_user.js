05b33a444c5e7b4cbc83f4d448d5617aeac19f3d
tam.vu <vuthanhtam1506@gmail.com>
2020-12-14 17:05:15 +0700

    Fix fetch user

--- services/subscription/create/fetch_user.js
+++ services/subscription/create/fetch_user.js
@@ -1,9 +1,14 @@
-// import { getUserData } from "../../sharetribe_admin";
-
 import { getUserData } from "../../sharetribe_admin";
+import { stripe } from "../../stripe";
 
 const fetchCustomer = async (userId) => {
-  return getUserData({ userId });
+  return Promise.all([
+    getUserData({ userId, include: ['stripeAccount'] }),
+    stripe.jh.customer.find(userId)
+  ]).then(([customer, stripeCustomer]) => {
+    customer.stripeCustomer = stripeCustomer;
+    return customer;
+  })
 }
 
 export default fetchCustomer;
