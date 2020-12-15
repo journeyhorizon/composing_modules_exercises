d4730ccce5dd982e02adc96fdbf262ce8160831b
trangpham-jh <trang.pham@journeyh.io>
2020-12-15 09:17:04 +0700

    Change name of table in config dynamodb

--- services/admin/company/enterprise/cancel/remove_item_dynamo.js
+++ services/admin/company/enterprise/cancel/remove_item_dynamo.js
@@ -3,7 +3,7 @@ import dynamoDBSdk from '../../../../dynamoDB';
 
 const removeItemInDynamoDB = (fnParams) => async () => {
   const { customerId } = fnParams;
-  return dynamoDBSdk(config.aws.dynamodb.tableName).delete({ id: customerId });
+  return dynamoDBSdk(config.aws.dynamodb.enterpriseTable).delete({ id: customerId });
 }
 
 export default removeItemInDynamoDB;
--- services/admin/company/enterprise/change/update_item_dynamo.js
+++ services/admin/company/enterprise/change/update_item_dynamo.js
@@ -3,7 +3,7 @@ import dynamoDBSdk from '../../../../dynamoDB';
 
 const updateItemInDynamoDB = (fnParams) => async () => {
   const { customerId, quantity } = fnParams;
-  return dynamoDBSdk(config.aws.dynamodb.tableName).put({ id: customerId, quantity });
+  return dynamoDBSdk(config.aws.dynamodb.enterpriseTable).put({ id: customerId, quantity });
 }
 
 export default updateItemInDynamoDB;
