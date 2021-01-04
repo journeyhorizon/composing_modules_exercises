0db558d33fc6f474006b8e84aa6310db80e094df
tam.vu <vuthanhtam1506@gmail.com>
2021-01-04 17:17:21 +0700

    update logging

--- routes/internalApi/events/index.js
+++ routes/internalApi/events/index.js
@@ -6,14 +6,14 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', handleAsyncWrapper(async (req, res, next) => {
+  //TODO:JH remove this log after we have confirm AWS hook from prod
+  console.log(req.body);
   const body = typeof req.body === 'string'
     ? JSON.parse(req.body)
     : req.body;
   const params = typeof body.Message === 'string'
     ? JSON.parse(body.Message)
     : body.Message;
-  //TODO:JH remove this log after we have confirm AWS hook from prod
-  console.log(params)
   const result = await eventSDK.jobs.updateListings(params);
   return res.status(result.code).send(result.data);
 }, { retries: config.retries }));
