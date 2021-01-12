93db55713ff160c5ea4938669760d3539862b672
tam.vu <vuthanhtam1506@gmail.com>
2021-01-12 13:43:24 +0700

    Remove unused res, update template config

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import { handleAsyncWrapper } from '../services/request_handle_wrapper';
 import config from '../services/config';
 
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
@@ -36,11 +35,5 @@ router.get('/', handleAsyncWrapper(async (req, res, next) => {
 }, { retries: config.retries }));
 
 router.use('/v1', require('./flex'));
-
-router.use(express.json());
-router.use(express.urlencoded({ extended: false }));
-router.use(cookieParser());
-router.use('/api', require('./api'));
-
 
 module.exports = router;
--- services/config/default_configuration.js
+++ services/config/default_configuration.js
@@ -7,27 +7,13 @@ const sharetribeFlex = {
     'https://flex-api.sharetribe.com',
   integration: {
     clientId: process.env.SHARETRIBE_FLEX_INTEGRATION_CLIENT_ID,
-    secret: process.env.SHARETRIBE_FLEX_INTEGRATION_SECRET_ID,
-    interval: {
-      updateListings: process.env.SHARETRIBE_FLEX_INTERVAL_UPDATE_LISTINGS
-        ? parseInt(process.env.SHARETRIBE_FLEX_INTERVAL_UPDATE_LISTINGS)
-        : 60000,
-    }
+    secret: process.env.SHARETRIBE_FLEX_INTEGRATION_SECRET_ID
   },
   clientSdk: {
     clientId: process.env.SHARETRIBE_SDK_CLIENT_ID,
     secret: process.env.SHARETRIBE_SDK_SECRET_ID,
     transitVerbose: process.env.SHARETRIBE_SDK_TRANSIT_VERBOSE === 'true'
-  },
-  page: {
-    defaultEmailPrefix: process.env.DEFAULT_PAGE_EMAIL_PREFIX,
-    defaultEmailSuffix: process.env.DEFAULT_PAGE_EMAIL_SUFFIX,
-    secret: process.env.PASSWORD_SECRET,
-    teamVerificationExpireTime: '7 days'
-  },
-  marketplaceName: env === 'production'
-    ? "Ship Shop"
-    : "Ship Shop Test"
+  }
 };
 
 const stripe = {
@@ -37,59 +23,20 @@ const stripe = {
   secret: process.env.STRIPE_SECRET
 };
 
-const email = {
-  senderAddress: process.env.SENDER_EMAIL_ADDRESS
-    ? process.env.SENDER_EMAIL_ADDRESS
-    : "tri.nguyen@journeyh.io",
-  adminAddress: process.env.ADMIN_EMAIL_ADDRESS,
-};
-
-const sms = {
-  accountSid: process.env.SMS_ACCOUNT_SID,
-  authToken: process.env.SMS_AUTH_TOKEN,
-  hostedNumber: process.env.SMS_HOSTED_NUMBER,
-}
-
-const aws = {
-  ses: {
-    region: process.env.AWS_SES_REGION
-  },
-  region: process.env.SERVER_AWS_REGION,
-  dynamodb: {
-    enterpriseTable: process.env.ENTERPRISE_CUSTOMER_TABLE,
-  },
-};
-
 const webCanonicalUrl = process.env.WEB_CANONICAL_URL;
 
-const enableSubscription = process.env.ENABLE_SUBSCRIPTION === 'true';
-
-const subscription = enableSubscription
-  ? {
-    enable: true,
-    endSubscriptionTrialImmediately: env !== 'production' &&
-      process.env.STRIPE_SHOULD_END_SUBSCRIPTION_TRIAL_IMMEDIATELY === 'true'
-      ? true
-      : false,
-    trialPeriod: process.env.STRIPE_CUSTOMER_TRIAL_PERIOD
-      ? parseInt(process.env.STRIPE_CUSTOMER_TRIAL_PERIOD)
-      : null,
-    endSubscriptionImmediately: env !== 'production' &&
-      process.env.STRIPE_SHOULD_END_SUBSCRIPTION_IMMEDIATELY === 'true',
-  }
-  : {
-    enable: false
-  };
+const subscription = {
+  trialPeriod: process.env.STRIPE_CUSTOMER_TRIAL_PERIOD
+    ? parseInt(process.env.STRIPE_CUSTOMER_TRIAL_PERIOD)
+    : null,
+};
 
 const config = {
   env,
   nodeEnv,
   retries,
   sharetribeFlex,
-  email,
-  aws,
   webCanonicalUrl,
-  sms,
   stripe,
   subscription
 }
