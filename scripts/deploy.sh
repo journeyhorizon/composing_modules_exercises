08c2f9b05525dd4d52e2553904773289a3ce7db7
tam.vu <vuthanhtam1506@gmail.com>
2022-07-15 11:32:34 +0700

    only run ssh when it's test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,10 @@ docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_INSTANCE_REGI
 echo -e "${COLOR}::::pushing to aws repo::::${NC}"
 docker push ${AWS_ECR_REPO_URL}
 
-echo -e "${COLOR}::::ssh and deploy::::${NC}"
-ssh -o StrictHostKeyChecking=no -i "${AWS_PRIVATE_KEY_PATH}" ${AWS_INSTANCE_URL} "IMAGE_URL=${AWS_ECR_REPO_URL} REGION=${AWS_INSTANCE_REGION} ${AWS_INSTANCE_DEPLOY_SCRIPT}"
+if [[ "$CIRCLE_BRANCH" == "main" ]] || [[ "$CIRCLE_BRANCH" == "master" ]] ; then
+  echo -e "${COLOR}::::ssh and deploy::::${NC}"
+  ssh -o StrictHostKeyChecking=no -i "${AWS_PRIVATE_KEY_PATH}" ${AWS_INSTANCE_URL} "IMAGE_URL=${AWS_ECR_REPO_URL} REGION=${AWS_INSTANCE_REGION} ${AWS_INSTANCE_DEPLOY_SCRIPT}"
+fi
 
 if [ "$CIRCLECI" == "true" ]; then
   echo -e "${COLOR}:::::::::Deployed by the CI:::::::::${NC}"
